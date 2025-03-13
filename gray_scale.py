a3c2b50716da020ad2b5d0a1506aa6d8388f6368
divya05shree <divyashreershettigar@gmail.com>
2025-03-13 10:22:40 +0530

    removed old files

--- gray_scale.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np 
-image=cv2.imread("website.jpg")
-cv2.imshow('original',image)
-cv2.waitKey(0) 
-gray_image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-cv2.imshow('grayscale',gray_image)
-cv2.waitKey(0) 
-cv2.destroyAllWindows()
